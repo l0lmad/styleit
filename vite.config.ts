242d030bf1b9e764920b9d15b768b2a9a9c9458f
Wara Wear <admin@warawear.com>
2026-06-02 20:33:41 +0300

    Update base path for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/interactive-clothing-e-commerce-website/",
+  base: "/warawear/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
