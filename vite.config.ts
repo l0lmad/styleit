18e971c08a506dd2a0d653ed1226fa76fa5f991e
Wara Wear <admin@warawear.com>
2026-07-04 12:38:42 +0300

    change base path to styleit

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/warawear/",
+  base: "/styleit/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
